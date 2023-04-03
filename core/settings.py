d8f7818ace84b32c5bdb759c617c2b451632fae8
Ricardo Pereira <patresio@gmail.com>
2023-04-03 11:00:56 -0300

    Arrumando o staticfiles

--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('POSTGRES_DB', 'sisinfo_db'),
@@ -95,7 +95,7 @@ DATABASES = {
         'PORT': config('DB_PORT', 5431, cast=int),
     },
 }
- """
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -139,7 +139,7 @@ STATIC_ROOT = os.path.join('staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
