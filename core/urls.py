89e9f56541f6fc94288b9743b681bcfca4e3eaf2
Ricardo Pereira <patresio@gmail.com>
2023-04-04 15:59:51 -0300

    Arrumando o staticfiles - ajustes

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
     path('', include('dashboard.urls')),
     path('diretorias/', include('diretorias.urls')),
